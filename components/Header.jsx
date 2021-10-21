777cc94af8e662f480d4ae8b074e60a8a7300157
shafaet-newgen <shafaet@newgen-bd.com>
2021-10-21 17:18:37 +0600

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,50 @@
+import Home from "@mui/icons-material/Home";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import { styled } from "@mui/system";
+import MuiNextLink from "../components/MuiNextLink";
+import Navbar from "./Navbar";
+import SideDrawer from "./SideDrawer";
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
+
+export const navLinks = [
+  { title: "home", path: "/" },
+  { title: "about us", path: "/about-us" },
+  { title: "menu", path: "/menu" },
+  { title: "catering", path: "/catering" },
+  { title: "contact", path: "/contact" },
+];
+
+const Header = () => {
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Container
+            maxWidth="lg"
+            sx={{ display: `flex`, justifyContent: `space-between` }}
+          >
+            <IconButton edge="start" aria-label="home">
+              <MuiNextLink activeClassName="active" href="/">
+                <Home
+                  sx={{
+                    color: (theme) => theme.palette.common.white,
+                  }}
+                  fontSize="large"
+                />
+              </MuiNextLink>
+            </IconButton>
+            <Navbar navLinks={navLinks} />
+            <SideDrawer navLinks={navLinks} />
+          </Container>
+        </Toolbar>
+      </AppBar>
+
+      <Offset />
+    </>
+  );
+};
+
+export default Header;
